10e2c1bbefadf56ff5971951a56b5612082dbf00
hubmartin <hub.martin@gmail.com>
2019-10-10 11:58:23 +0200

    Cleanup, rename to infragrid-sensor

--- app/application.c
+++ app/application.c
@@ -21,9 +21,7 @@ uint8_t timetable_index;
 bool timetable_force_reload = false;
 
 bc_tick_t pir_deadtime_timestamp = 0;
-
 bc_scheduler_task_id_t infragrid_task_id;
-
 
 float map_f(float x, float in_min, float in_max, float out_min, float out_max)
 {
@@ -39,42 +37,6 @@ float map_f(float x, float in_min, float in_max, float out_min, float out_max)
     }
 
     return val;
-}
-
-uint16_t bit_packer(uint8_t *src, uint16_t src_size, uint8_t *dst, uint16_t dst_size, uint8_t bit_count)
-{
-    memset(dst, 0x00, dst_size);
-
-    uint32_t bit = 0;
-    uint16_t max_dst_index = 0;
-    for (int i = 0; i < src_size; i++)
-    {
-        uint8_t val = (uint8_t)src[i];
-        uint32_t b = bit / 8;
-        uint32_t offset = bit % 8;
-        if (offset > 2)
-        {
-            max_dst_index = b + 1;
-            if (max_dst_index > dst_size)
-            {
-                return 0;
-            }
-            dst[b] |= val << offset;
-            dst[b+1] |= val >> (8-offset);
-        }
-        else
-        {
-            max_dst_index = b;
-            if (max_dst_index > dst_size)
-            {
-                return 0;
-            }
-            dst[b] |= val << offset;
-        }
-        bit += bit_count;
-    }
-
-    return max_dst_index;
 }
 
 size_t compress(uint8_t *in, size_t in_size, uint8_t *buffer, size_t buffer_size)
@@ -154,7 +116,7 @@ void bc_module_infra_grid_log(float *temperature)
 {
     for (int i = 0; i < 8; i++)
     {
-        bc_log_debug("%2.1f %2.1f %2.1f %2.1f %2.1f %2.1f %2.1f %2.1f", 
+        bc_log_debug("%2.1f %2.1f %2.1f %2.1f %2.1f %2.1f %2.1f %2.1f",
             temperatures[8*i + 0], temperatures[8*i + 1], temperatures[8*i + 2], temperatures[8*i + 3],
             temperatures[8*i + 4], temperatures[8*i + 5], temperatures[8*i + 6], temperatures[8*i + 7]);
     }
@@ -169,7 +131,7 @@ void bc_module_infra_grid_log2(float *temperature)
 
     for (int i = 0; i < 8; i++)
     {
-        bc_log_debug("%c%c%c%c%c%c%c%c", 
+        bc_log_debug("%c%c%c%c%c%c%c%c",
             c[ (uint8_t)map_f(temperatures[8*i + 0], in_min, in_max, 0, sizeof(c)) ],
             c[ (uint8_t)map_f(temperatures[8*i + 1], in_min, in_max, 0, sizeof(c)) ],
             c[ (uint8_t)map_f(temperatures[8*i + 2], in_min, in_max, 0, sizeof(c)) ],
@@ -204,7 +166,7 @@ void infragrid_event_handler(bc_module_infra_grid_t *self, bc_module_infra_grid_
     {
         bc_log_debug("INFRA UPDATE");
         bc_module_infra_grid_get_temperatures_celsius(self, temperatures);
-        
+
         bc_module_infra_grid_log(temperatures);
         bc_module_infra_grid_log2(temperatures);
 
@@ -222,13 +184,7 @@ void infragrid_event_handler(bc_module_infra_grid_t *self, bc_module_infra_grid_
             src[i] = (uint8_t)temp;
         }
 
-        //uint16_t dst_size = bit_packer(src, sizeof(src), dst, sizeof(dst), 6);
         uint16_t dst_size = compress(src, sizeof(src), dst, sizeof(dst));
-
-        /*static volatile uint8_t dec[100];
-        decompress(dec, sizeof(dec), dst, dst_size);*/
-
-        bc_log_debug("bit_packer dst_size: %d", dst_size);
 
         if (dst_size)
         {
@@ -252,7 +208,6 @@ void battery_event_handler(bc_module_battery_event_t event, void *event_param)
     }
 }
 
-
 void application_init(void)
 {
     // Initialize logging
@@ -263,8 +218,8 @@ void application_init(void)
     bc_led_pulse(&led, 2000);
 
     // Initialize button
-    //bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
-    //bc_button_set_event_handler(&button, button_event_handler, NULL);
+    bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
+    bc_button_set_event_handler(&button, button_event_handler, NULL);
 
     // Infra Grid Module
     bc_module_infra_grid_init(&infra);
@@ -278,5 +233,5 @@ void application_init(void)
 
     // Radio init
     bc_radio_init(BC_RADIO_MODE_NODE_SLEEPING);
-    bc_radio_pairing_request("printer-desk-monitor", VERSION);
+    bc_radio_pairing_request("infragrid-sensor", VERSION);
 }
